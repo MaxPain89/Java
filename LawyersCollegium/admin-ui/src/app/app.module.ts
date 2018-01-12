c6044b41a245d3f103fdbf0f03ae410e6610abac
Maxim Kalitinenkov <mkalitinenkov@hystax.com>
2018-01-12 10:50:53 +0300

    Added table with sorting, pagination and filtering.

--- LawyersCollegium/admin-ui/src/app/app.module.ts
+++ LawyersCollegium/admin-ui/src/app/app.module.ts
@@ -1,14 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes} from "@angular/router";
-import { HttpClientModule } from "@angular/common/http";
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
 
 
-import { AppComponent } from './app.component';
-import { DecreeComponent } from './components/decree/decree.component';
-import { LawyerComponent } from './components/lawyer/lawyer.component';
+import {AppComponent} from './app.component';
+import {DecreeComponent} from './components/decree/decree.component';
+import {LawyerComponent} from './components/lawyer/lawyer.component';
 
-import { DecreeService } from "./services/decree.service";
+import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule, MatInputModule, MatPaginatorModule} from '@angular/material';
+import {MatSortModule} from '@angular/material/sort';
+
+import {DecreeService} from "./services/decree.service";
 
 const appRoutes: Routes = [
   {path:"decrees", component: DecreeComponent},
@@ -24,7 +29,13 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [DecreeService],
   bootstrap: [AppComponent]
