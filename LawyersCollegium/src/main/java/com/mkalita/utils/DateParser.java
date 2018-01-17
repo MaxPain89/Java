8921af7cdf10a8e3ac4edf5629b511d4dbf40da9
Maxim Kalitinenkov <mkalitinenkov@hystax.com>
2018-01-17 15:09:39 +0300

    Fixed bug with unsafe using date format with static

--- LawyersCollegium/src/main/java/com/mkalita/utils/DateParser.java
+++ LawyersCollegium/src/main/java/com/mkalita/utils/DateParser.java
@@ -7,15 +7,15 @@ import java.util.Date;
 
 public class DateParser {
     private static final String datePattern = "dd/MM/yyyy";
-    private static final DateFormat df = new SimpleDateFormat(datePattern);
+    private static final ThreadLocal<DateFormat> df = ThreadLocal.withInitial(() -> new SimpleDateFormat(datePattern));
 
     public static String dateToStr(Date date) {
-        return df.format(date);
+        return df.get().format(date);
     }
 
     public static Date dateFromStr(String strDate) {
         try {
-            return df.parse(strDate);
+            return df.get().parse(strDate);
         } catch (ParseException e) {
             throw new RuntimeException(String.format("Couldn't parse date: %s", strDate));
         }
