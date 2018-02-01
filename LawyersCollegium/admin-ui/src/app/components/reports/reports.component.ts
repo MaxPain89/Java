f3da2518150126199f7938e1a7e447750c51ab7e
Maxim Kalitinenkov <mkalitinenkov@hystax.com>
2018-02-01 11:04:52 +0300

    Added page for reports

--- LawyersCollegium/admin-ui/src/app/components/reports/reports.component.ts
+++ LawyersCollegium/admin-ui/src/app/components/reports/reports.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ReportService} from "../../services/report.service";
+import {CollegiumService} from "../../services/collegium.service";
+import {Moment} from "moment";
+import {MatDatepickerInputEvent} from "@angular/material";
 
 @Component({
   selector: 'app-reports',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+  collegiumsMaps = {};
+  currentCollegiumId = 0;
+  currentReportDecreeDate: Moment;
+  constructor(private reportService: ReportService,
+              private collegiumService: CollegiumService) { }
 
   ngOnInit() {
+    this.getCollegiumMap();
+  }
+
+  getCollegiumMap() {
+    this.collegiumService.getCollegiums().subscribe(collegiumsResp => {
+      let collegiumsMap = {};
+      collegiumsResp.forEach(function (collegium) {
+        collegiumsMap[collegium.id] = collegium;
+      });
+      this.collegiumsMaps = collegiumsMap;
+    })
+  }
+
+  getCollegiumsMapKeys() : Array<number> {
+    return Object.keys(this.collegiumsMaps).map(Number);
+  }
+
+  onChangeCollegium(value) {
+
+  }
+
+  onChangeReportDateDate(event: MatDatepickerInputEvent<Moment>) {
+    this.currentReportDecreeDate = event.value;
+  }
+
+  open() {
+    this.reportService.getDecreeReport(this.currentCollegiumId, false, this.currentReportDecreeDate.format("DD/MM/YYYY"));
+  }
+
+
+  save() {
+    this.reportService.getDecreeReport(this.currentCollegiumId, true, this.currentReportDecreeDate.format("DD/MM/YYYY"));
   }
 
 }
